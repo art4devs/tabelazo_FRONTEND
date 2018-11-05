531ec35de641255e14f22053968eae4e6f2934f0
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2018-11-05 16:43:51 -0200

    configurando layout e criando loginComponent

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {LoginComponent} from './login/login.component';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [CommonModule],
+  exports: [LoginComponent]
+})
+export class AuthModule { }
